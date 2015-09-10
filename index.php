ebf49b08d77dc0c2c137310124c4649bb6a32867
root <root@172.31.9.189>
2015-09-10 04:39:08 +0000

    2nd commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $json = file_get_contents('php://input');
 $params = json_decode($json, true);
-error_log(print_r($params, true), 3, 'error.log');
+error_log(print_r($params, true), 3, 'errors.log');
