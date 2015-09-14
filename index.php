b9a20ea640e3164905a07b246d817a00e045d6e3
root <root@172.31.9.189>
2015-09-14 06:30:03 +0000

    commit multi 1000

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-
 echo "<h1>Hello SQL world 222</h1>";
-$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
-if ($mysqli->connect_error) {
-    exit($mysqli->connect_error);
-} else {
-    $mysqli->set_charset("utf8");
-}
-$result = $mysqli->query("select 1") or exit($mysqli->error());
-echo var_dump($result->fetch_assoc());
-$mysqli->close();
-
 ?>
