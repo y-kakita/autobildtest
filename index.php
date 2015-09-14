e6569634d4e6c3520e910be165078a1645bec5fb
root <root@172.31.9.189>
2015-09-14 05:40:05 +0000

    sql commit 1

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
-$json = file_get_contents('php://input');
-$params = json_decode($json, true);
-error_log(print_r($params, true), 3, 'error.log');
-echo "Hello change world 37  !!";
-echo "kotiramokotiramo";
-?>
 
+echo "<h1>Hello SQL world</h1>";
+$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+if ($mysqli->connect_error) {
+    exit($mysqli->connect_error);
+} else {
+    $mysqli->set_charset("utf8");
+}
+$result = $mysqli->query("select 1") or exit($mysqli->error());
+echo var_dump($result->fetch_assoc());
+$mysqli->close();
+
+?>
